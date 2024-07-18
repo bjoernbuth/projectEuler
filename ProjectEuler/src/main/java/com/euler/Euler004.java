b9a5c30cb9c966074aa5fa3429dd9a718cdb7ef7
bjoernbuth <bjoernbuth76@gmail.com>
2024-07-18 14:08:05 +0200

    changed some class names after files were renamed

--- ProjectEuler/src/main/java/com/euler/Euler004.java
+++ ProjectEuler/src/main/java/com/euler/Euler004.java
@@ -2,30 +2,33 @@ package com.euler;
 
 import com.euler.common.Timing;
 
-public class Euler4 {
-	private static boolean isPalindrome(long in)	{
-		long[] digits=new long[6];
-		for (int i=0;i<6;++i)	{
-			digits[i]=in%10;
-			in/=10;
+public class Euler004 {
+	private static boolean isPalindrome(long in) {
+		long[] digits = new long[6];
+		for (int i = 0; i < 6; ++i) {
+			digits[i] = in % 10;
+			in /= 10;
 		}
-		return ((digits[0]==digits[5])&&(digits[1]==digits[4])&&(digits[2]==digits[3]));
+		return ((digits[0] == digits[5]) && (digits[1] == digits[4]) && (digits[2] == digits[3]));
 	}
-	
-	private static long solve()	{
-		long bestFound=-1;
-		for (long a=990;;a-=11l)	{
-			if (a*999<bestFound) break;
-			for (long b=999;b>0;--b)	{
-				long product=a*b;
-				if (product<bestFound) break;
-				else if (isPalindrome(product)) bestFound=product;
+
+	private static long solve() {
+		long bestFound = -1;
+		for (long a = 990;; a -= 11l) {
+			if (a * 999 < bestFound)
+				break;
+			for (long b = 999; b > 0; --b) {
+				long product = a * b;
+				if (product < bestFound)
+					break;
+				else if (isPalindrome(product))
+					bestFound = product;
 			}
 		}
 		return bestFound;
 	}
 
-	public static void main(String[] args)	{
-		Timing.time(Euler4::solve);
+	public static void main(String[] args) {
+		Timing.time(Euler004::solve);
 	}
 }
--- ProjectEuler/src/main/java/com/euler/Euler018.java
+++ ProjectEuler/src/main/java/com/euler/Euler018.java
@@ -11,48 +11,53 @@ import java.util.stream.Collectors;
 
 import com.euler.common.Timing;
 
-public class Euler18 {
+public class Euler018 {
 	private static String fileName;
-	
-	private static int[][] parseLines(List<String> lines)	{
-		int[][] result=new int[lines.size()][];
-		result[0]=new int[] {Integer.parseInt(lines.get(0))};
-		for (int i=1;i<lines.size();++i)	{
-			String[] split=lines.get(i).split(" ");
-			if (split.length!=i+1) throw new IllegalArgumentException();
-			result[i]=new int[i+1];
-			for (int j=0;j<=i;++j) result[i][j]=Integer.parseInt(split[j]);
+
+	private static int[][] parseLines(List<String> lines) {
+		int[][] result = new int[lines.size()][];
+		result[0] = new int[] { Integer.parseInt(lines.get(0)) };
+		for (int i = 1; i < lines.size(); ++i) {
+			String[] split = lines.get(i).split(" ");
+			if (split.length != i + 1)
+				throw new IllegalArgumentException();
+			result[i] = new int[i + 1];
+			for (int j = 0; j <= i; ++j)
+				result[i][j] = Integer.parseInt(split[j]);
 		}
 		return result;
 	}
-	
-	private static long[][] getBestSums(int[][] in)	{
-		long[][] result=new long[in.length][];
-		result[0]=new long[] {in[0][0]};
-		for (int i=1;i<in.length;++i)	{
-			result[i]=new long[i+1];
-			result[i][0]=result[i-1][0]+in[i][0];
-			for (int j=1;j<i;++j) result[i][j]=Math.max(result[i-1][j-1],result[i-1][j])+in[i][j];
-			result[i][i]=result[i-1][i-1]+in[i][i];
+
+	private static long[][] getBestSums(int[][] in) {
+		long[][] result = new long[in.length][];
+		result[0] = new long[] { in[0][0] };
+		for (int i = 1; i < in.length; ++i) {
+			result[i] = new long[i + 1];
+			result[i][0] = result[i - 1][0] + in[i][0];
+			for (int j = 1; j < i; ++j)
+				result[i][j] = Math.max(result[i - 1][j - 1], result[i - 1][j]) + in[i][j];
+			result[i][i] = result[i - 1][i - 1] + in[i][i];
 		}
 		return result;
 	}
-	
-	private static long solve()	{
-		try	{
-			URL resource=Euler18.class.getResource(fileName);
-			List<String> lines=Files.lines(Paths.get(resource.toURI())).collect(Collectors.toList());
-			int[][] triangle=parseLines(lines);
-			long[][] bestSums=getBestSums(triangle);
-			return Arrays.stream(bestSums[bestSums.length-1]).max().getAsLong();
-		}	catch (IOException|URISyntaxException exc)	{
+
+	private static long solve() {
+		try {
+			URL resource = Euler018.class.getResource(fileName);
+			List<String> lines = Files.lines(Paths.get(resource.toURI())).collect(Collectors.toList());
+			int[][] triangle = parseLines(lines);
+			long[][] bestSums = getBestSums(triangle);
+			return Arrays.stream(bestSums[bestSums.length - 1]).max().getAsLong();
+		} catch (IOException | URISyntaxException exc) {
 			throw new RuntimeException(exc);
 		}
 	}
 
-	public static void main(String[] args)	{
-		if (args.length==0) fileName="in18.txt";
-		else fileName="in67.txt";
-		Timing.time(Euler18::solve);
+	public static void main(String[] args) {
+		if (args.length == 0)
+			fileName = "in18.txt";
+		else
+			fileName = "in67.txt";
+		Timing.time(Euler018::solve);
 	}
 }
